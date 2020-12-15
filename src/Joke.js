e6fd9f8ca2e1ba092a456be69e0ff6ff1c5ad4cb
jkunciene <j.kunciene@gmail.com>
2020-12-15 19:07:23 +0200

    be useEffect

--- src/Joke.js
+++ src/Joke.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios'
 
 const Joke = () => {
+
+    //TOP LEVEL for STATE
+const [joke, setJoke] = useState([])
+
+//duomenu gavimo metodas-funkcija
+const getJoke=()=>{
+    axios.get('https://evilinsult.com/generate_insult.php?lang=en&type=json')
+    .then((response)=>{
+        console.log(response)
+        let item = response.data
+        setJoke(item)
+        
+    })
+}
+
+
+console.log(joke)
+
     return (
-        <div>
-            muzika
+        <div>            
+            {joke.insult}
+            <button onClick={getJoke}>Get your joke...</button>
         </div>
     )
 }
